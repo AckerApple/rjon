1d544063389aa4836131e8a26a160842f38b5483
ackerapple <acker.dawn.apple@gmail.com>
2018-01-22 10:20:28 -0500

    x

--- dist/RjonModule.js
+++ dist/RjonModule.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//import { Ng2PageScrollModule } from 'ng2-page-scroll';
 var platform_browser_1 = require("@angular/platform-browser");
 var core_1 = require("@angular/core");
 var common_1 = require("@angular/common");
 var forms_1 = require("@angular/forms");
 var http_1 = require("@angular/http");
-//import { providers } from "./providers"
 var components_1 = require("./components");
 var ack_angular_1 = require("ack-angular");
 var RjonModule = (function () {
